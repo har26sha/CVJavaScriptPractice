7362a32461a888540b29930574498cb4262df649
har26sha <hv73245@gmail.com>
2026-04-14 09:45:11 +0530

    update2

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-let myname="riya";
-let age="23";
-console.log("My name is "+myname+" and my age is "+age);
-console.log(`My name is ${myname} and my age is ${age}`);
+// let myname="riya";
+// let age="23";
+// console.log("My name is "+myname+" and my age is "+age);
+// console.log(`My name is ${myname} and my age is ${age}`);
+
+//console.log(document.body);
+
+// let first=document.getElementById('first');
+// //console.log(first.innerText);
+// console.log(first.innerHTML="<b>hello</b>");
+
+let boxes=document.getElementsByClassName('box');
+console.log(boxes);
+boxes[1].innerHTML="boy";
+document.getElementsByClassName('para')[0].innerHTML="hola";
